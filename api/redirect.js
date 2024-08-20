ebf804051137f8bb4e1b43d57b65a2dd091addcc
Pankaj Yadav <pankaj.y@rejolut.com>
2024-08-20 23:25:32 +0530

    redirection

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,9 @@
 module.exports = (req, res) => {
-  res.writeHead(302, {
-    Location: "tagocash%3A%2F%2Fpublic%2Fcontinue_with_email",
-  });
-  res.end();
+  const redirectUrl = req.query.redirectUrl;
+  if (redirectUrl) {
+    res.writeHead(302, { Location: redirectUrl });
+    res.end();
+  } else {
+    res.status(400).send("Redirect URL is missing");
+  }
 };
